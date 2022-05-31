d126f9fd1b67f10bf348bf588b80870dae594331
wayr8 <92333451+wayr8@users.noreply.github.com>
2022-05-31 14:44:37 +0300

    Update TText.h

--- text/TText.h
+++ text/TText.h
@@ -40,74 +40,46 @@ class TText
 {
 private:
 	TNode *pFirst;
-	TNode *pCurr;
-	//В стеке сохраняются все указатели до текущего (текущий не хранится)
-	//При перемещениях стек модифицируем
+	TNode *pCurr;	
 	TStack<TNode*> st;
 
-	//Рекурсивное чтение из файла
 	TNode* ReadRec(ifstream& fin);
-
-	//Печать на экран текста
-	//печатает саму строку, потом всё по pDown, потом всё по pNext
-	//(рекурсивно)
+	
 	int textLevel = 0;
 
 	void PrintRec(TNode* p);
 	void WriteRec(TNode* p, ostream& out);
 
 	TNode* CopyNode(TNode* p);
-
 public:
 	TText();
 	TText(TNode* p);
-	//Функция копирования; вызывает рекурсивную функцию копирования
+
 	TText* GetCopy();
-	//Перемещение указателя pCurr на следующее звено
 	void GoNextNode();
-	//Перемещение указателя pCurr на звено подчинённой ему части
 	void GoDownNode();
-	//Переход назад (вверх) по вложенности
 	void GoUp();
-	//Возврат в корневую (самую первую) строку всего текста
 	void GoFirstNode();
 
-	//Вставка новой строки за текущей
 	void InsNextLine(char* _str);
-	//Вставка нового заголовка за текущей строкой и
-	//переподчинение всей нижней части списка ему
 	void InsNextSection(char* _str);
-	//Вставка новой строки в начало подчинённой части
 	void InsDownLine(char* _str);
-	//Вставка нового заголовка в подчинённую часть
 	void InsDownSection(char* _str);
 
-	//С утечкой памяти (удаляется только pDel, но не его внутренности)!!!
-	//Удалить следующее звено
+	void Reset();
+	void GoNext();
+	bool IsEnd();
+	
 	void DelNext();
 	void DelDown();
-
-	//Возврат pCurr на первое звено
-	void Reset();
-	//Переход pCurr далее
-	void GoNext();
-	//Проверка окончания обхода
-	bool IsEnd();
-
-	//Получение строки текущего звена
+	
 	char* GetCurrentLine();
 
-	//Загрузка текста из файла
 	void Load(string fn);
-	//Печать текста на экран
 	void Print();
-	//Сохранение текста в файл
 	void Save(string fn);
-	//Пометка текущего звена как "не мусор"
 	void NotGarbage();
 };
-
-//................................................................
 
 TNode::TNode(char* _str, TNode* _pNext, TNode* _pDown)
 {
@@ -124,44 +96,28 @@ void* TNode::operator new(size_t size)
 	TNode* tmp = mem.pFree;
 
 	if (tmp == nullptr)
-		throw "Out of memory";
+		throw "Exception";
 
 	mem.pFree = mem.pFree->pNext;
 
-	//ВОЗМОЖНО, ПРИДЁТСЯ ЧТО-ТО ДОПИСАТЬ
 	return tmp;
 }
 
 void TNode::operator delete(void* ptr)
 {
 	TNode* tmp = mem.pFree;
-	//Запомнили освобождённый
 	TNode* p1 = (TNode*)ptr;
-	//Новый освобождённый теперь показывает на следующий свободный
 	p1->pNext = tmp;
-	//Обновим первый свободный
 	mem.pFree = p1;
-
-	//Оптимальнее (проверить):
-	//TNode* newpFree = (TNode*) ptr;
-	//newpFree->pNext = mem.pFree;
-	//mem.pFree = newpFree;
-
-	//ВОЗМОЖНО, ПРИДЁТСЯ ЧТО-ТО ДОПИСАТЬ
 }
 
 void TNode::InitMem(size_t size)
 {
-	//Выделение памяти под size элементов TNode (через костыль)
 	mem.pFirst = (TNode*) new char[size * sizeof(TNode)];
-
-	//Установка pFree
 	mem.pFree = mem.pFirst;
-	//Вычисление pLast через pFree
 	mem.pLast = mem.pFirst + (size - 1);
 
 	TNode* p = mem.pFirst;
-	//Расстановка pNext'ов ([ ] -> [ ] -> ... [ ] -> [ ])
 	for (int i = 0; i < size - 1; i++)
 	{
 		p->pNext = p + 1;
@@ -170,7 +126,6 @@ void TNode::InitMem(size_t size)
 
 		p += 1;
 	}
-	//Установка null для последнего
 	mem.pLast->pNext = nullptr;
 	mem.pLast->str[0] = 0;
 }
@@ -188,31 +143,22 @@ void TNode::PrintFreeNodes()
 
 void TNode::CleanMem(TText& t)
 {
-	//Проход по списку свободных, отметка всех свободных как "не мусор"
 	for (t.Reset(); !t.IsEnd(); t.GoNext())
 	{
-		//(pCurr->Garbage = false)
 		t.NotGarbage();
 	}
-
-	//Проход по списку занятых, отметка всех занятых как "не мусор"
 	TNode* p = TNode::mem.pFree;
 	while (p != nullptr)
 	{
 		p->Garbage = false;
 		p = p->pNext;
 	}
-
-	//Остальное - мусор, его нужно вернуть в список свободных
 	p = TNode::mem.pFirst;
 	for (p = TNode::mem.pFirst; p <= TNode::mem.pLast; p++)
 	{
 		if (p->Garbage)
 		{
-			//Это наш самописный delete
 			delete p;
-
-			//Сам дописал
 			p->Garbage = false;
 		}
 	}
@@ -431,8 +377,6 @@ void TText::Reset()
 void TText::GoNext()
 {
 	pCurr = st.Pop();
-
-	//Если не дошли до "фиктивной строки снизу стека"
 	if (pCurr != pFirst)
 	{
 		if (pCurr->pNext != nullptr)
@@ -468,7 +412,7 @@ void TText::Save(string fn)
 	ofstream out;
 	out.open(fn);
 
-	if (!out.is_open())	throw "Export exception!";
+	if (!out.is_open())	throw "Exception";
 
 	WriteRec(pFirst, out);
 }
